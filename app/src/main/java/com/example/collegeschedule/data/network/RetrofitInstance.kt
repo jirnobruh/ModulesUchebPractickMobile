68a6bc439291e1c8f2e93ac1134aeca51feedde4
Server\NOT/founD <jirnobruh@vk.com>
2026-01-30 18:57:42 +0700

    Create MainActivity and fix ip for emulator

--- app/src/main/java/com/example/collegeschedule/data/network/RetrofitInstance.kt
+++ app/src/main/java/com/example/collegeschedule/data/network/RetrofitInstance.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
     private val retrofit = Retrofit.Builder()
-        .baseUrl("http://192.168.0.25:5268/")
+        .baseUrl("http://10.0.2.2:5254/")
         .addConverterFactory(GsonConverterFactory.create())
         .build()
     val api: ScheduleApi = retrofit.create(ScheduleApi::class.java)
