aa4e85b749d25d3385a5fff5e8d7d3912387857c
Server\NOT/founD <jirnobruh@vk.com>
2026-01-30 17:37:47 +0700

    fix gradle. Data api and network packet edit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,8 +53,8 @@ dependencies {
     implementation(libs.androidx.compose.material3.adaptive.navigation.suite)
 
     // Сетевое взаимодействие (Retrofit)
-    implementation("com.squareup.retrofit2.retrofit:2.12.0")
-    implementation("com.squareup.retrofit2.converter-gson:2.12.0")
+    implementation("com.squareup.retrofit2:retrofit:2.12.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.12.0")
     // Управление состоянием UI (ViewModel для Compose) – опционально, если ктото захочет лезть в MVVM
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
     // Асинхронные операции (Корутины)
