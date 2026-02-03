8b9cb9e52f426062e5cde81705e23915b9a2d24d
Server\NOT/founD <jirnobruh@vk.com>
2026-02-03 16:10:54 +0700

    Fix patch to endpoint

--- app/src/main/java/com/example/collegeschedule/data/api/ScheduleApi.kt
+++ app/src/main/java/com/example/collegeschedule/data/api/ScheduleApi.kt
@@ -12,7 +12,6 @@ interface ScheduleApi {
         @Query("start") start: String,
         @Query("end") end: String
     ): List<ScheduleByDateDto>
-    @GET("api/groups")
+    @GET("api/schedule/groups")
     suspend fun getGroups(): List<String>
-
 }
